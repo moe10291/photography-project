49ee06c65cca7079b27532a67c49300d1169fead
moe10291 <mohammad57zero@gmail.com>
2019-03-01 18:22:55 -0500

    working on navbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,9 +3,12 @@ import pic2 from '../pic2.png';
 import pic6 from '../pic6.png';
 import pic8 from '../pic8.jpg';
 import pic10 from '../pic10.png';
+import b from '../b.png'
+
 import './Landing.css';
 // import Nav from '../Nav'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
 
 class Landing extends Component {
     constructor() {
@@ -16,25 +19,36 @@ class Landing extends Component {
     render() {
         return (
             <div>
-<h1>Hello World</h1>
-<Link to='/'>Home</Link>
-<br></br>
-<Link to='/about'>About Me</Link>
+                <span className='logo'>
+                <img className='b' src= {b} alt='b'/>
+                <h1>Bardia</h1>
+                <h2>Photography</h2>
+                </span>
+                 <div className='container'>
+                
+                    <ul>
+                <li><Link to='/' className='Link'>Home</Link></li>
+                
+                <li><Link to='/about' className='Link'>About Me</Link></li>
 
-<br></br>
-<Link to='/portfolio'>Portfolio</Link>
-<br></br>
-<Link to='/contact'>Contact</Link>
+                
+                <li><Link to='/portfolio' className='Link'>Portfolio</Link></li>
+                
+                <li><Link to='/contact' className='Link'>Contact</Link></li>
+                </ul>
+                
+                </div>
 
 
-<div id="slider">
-<figure>
-    <img src={pic2} alt="picture2"/>
-    <img src={pic6} alt="picture6"/>
-    <img src={pic8} alt="picture8"/>
-    <img src={pic10} alt="picture10"/>
-</figure>
-</div>
+                <div id="slider">
+                    <figure>
+                        <img src={pic2} alt="picture2" />
+                        <img src={pic6} alt="picture6" />
+                        <img src={pic8} alt="picture8" />
+                        <img src={pic10} alt="picture10" />
+                    </figure>
+                </div>
+
 
             </div>
         )
